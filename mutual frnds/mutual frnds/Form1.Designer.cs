4f70ac97db6d61124bd15a211774011864952b49
Maham imran <imaham9920@gmail.com>
2019-12-09 18:31:04 +0500

    Uploading full project

---
+++ mutual frnds/mutual frnds/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace mutual_frnds
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.suggest = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(55, 184);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(209, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.suggest});
+            this.dataGridView1.Location = new System.Drawing.Point(291, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(657, 204);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.RowPostPaint += new System.Windows.Forms.DataGridViewRowPostPaintEventHandler(this.dataGridView1_RowPostPaint);
+            // 
+            // suggest
+            // 
+            this.suggest.HeaderText = "suggest";
+            this.suggest.Name = "suggest";
+            this.suggest.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.suggest.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(84, 227);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 33);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Suggest Friends";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(291, 227);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(657, 233);
+            this.dataGridView2.TabIndex = 2;
+            this.dataGridView2.RowPostPaint += new System.Windows.Forms.DataGridViewRowPostPaintEventHandler(this.dataGridView1_RowPostPaint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(52, 157);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter no. of persons:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(960, 472);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewButtonColumn suggest;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ mutual frnds/mutual frnds/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mutual_frnds
+{
+    public partial class Form1 : Form
+    {
+        int numberOfPerson = -1;
+        List<person> personStore = new List<person>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private List<person>  xyz( person a)
+        {
+            List<person> test = new List<person>();
+            foreach (person x in personStore )
+            {
+                if (x.Age == a.Age || x.City == a.City || x.Name == a.Name || x.Uni == a.Uni)
+                    test.Add(x);
+            }
+            return test;
+        }
+        private void personGen(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Random r = new Random();
+                Thread.Sleep(1);
+                person temp = new person();
+                temp.Name = stringGen(4);
+                Thread.Sleep(1);
+                temp.City = stringGen(4);
+                Thread.Sleep(1);
+                temp.Age = stringGen(4);
+                Thread.Sleep(1);
+                temp.Uni = stringGen(4);
+                personStore.Add(temp);
+            }
+        }
+        private string stringGen(int len)
+        {
+            string temp = "";
+            Random r = new Random();
+            for (int i = 0; i < len; i++)
+            {
+                if (i == 0)
+                    temp = temp + (char)r.Next('A', 'E');
+                else
+                    temp = temp + (char)r.Next('a', 'e');
+            }
+            return temp;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Trim() != "")
+                numberOfPerson = Convert.ToInt32(textBox1.Text);
+            personStore.Clear();
+            if (numberOfPerson != -1)
+            {
+                personGen(numberOfPerson);
+                dataGridView1.DataSource = "";
+                dataGridView1.DataSource = personStore;
+                dataGridView1.Show();
+            }
+            else
+            {
+                MessageBox.Show("enter number of persons");
+            }
+
+
+            foreach(person x in personStore)
+            {
+                x.Suguestions = xyz(x);
+            }
+        }
+        private void dataGridView1_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
+        {
+            dataGridView1.Rows[e.RowIndex].Cells[0].Value = (e.RowIndex + 1);
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.ColumnIndex==0)
+            {
+                dataGridView2.DataSource = "";
+                dataGridView2.DataSource = personStore[e.RowIndex].Suguestions;
+                dataGridView2.Show();
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
